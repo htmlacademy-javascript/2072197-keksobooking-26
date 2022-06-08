4713e6bde9f87eb2d0706e495a17355e115d06c2
Keks <keks@htmlacademy.ru>
2022-06-08 13:17:28 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,27 @@
+function getRandomInteger(min, max) {
+  if (max < min) {
+    let changer = max;
+    max = min;
+    min = changer;
+  }
+  if (min === max) {
+    return min;
+  }
+  if ((min, max >= 0)) {
+    return Math.floor(Math.random() * (max + 1));
+  }
+}
+
+function getRandomDigits(min, max, digits) {
+  if (max < min) {
+    let changer = max;
+    max = min;
+    min = changer;
+  }
+  if (min === max) {
+    return min;
+  }
+  if ((min, max >= 0)) {
+    return (Math.random() * max).toFixed(digits);
+  }
+}
